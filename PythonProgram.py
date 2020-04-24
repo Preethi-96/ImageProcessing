11436c7c151665f941b70aed53230a30d981f86d
Preethi-96 <61061526+Preethi-96@users.noreply.github.com>
2020-04-25 03:09:07 +0530

    Update PythonProgram.py

--- PythonProgram.py
+++ PythonProgram.py
@@ -5,53 +5,52 @@ Created on Thu Feb  6 23:51:45 2020
 @author: User
 """
 import numpy as np
+from PIL import Image
 import pandas as pd
+
 import matplotlib.pyplot as plt
 #import opencv as cv2
-from PIL import Image
 def FileConverterFxn(datFile, imgHt,imgWdt, strFrame, EndFrame, x1,y1,x2,y2):
   fp=open(datFile, 'r')
   #print(fp.dtypes)
   fp.seek(0)
+  print(fp)
+  print(fp.tell())
   matrix=np.zeros((y2-y1+1, x2-x1+1, EndFrame-strFrame+1))
   #print(matrix)
   frameSize = 48 + (imgHt*imgWdt*2)
   d=1
-  for fr in range(strFrame,160):
+  def extents(f):
+      delta = f[1] - f[0]
+      return [f[0] - delta/2, f[-1] + delta/2]
+  for fr in range(strFrame,EndFrame):
      
       offset = 2060 + 48 + (frameSize * fr)
       fp.seek(offset)
+      #print(fp.tell())
       temp=np.zeros((imgHt,imgWdt))
       #print(temp)
-      temp1=np.fromfile(fp,dtype='>i2')
+      temp1=np.fromfile(fp,dtype='int16')
+ 
       temp=temp1[:imgHt*imgWdt].reshape((imgHt,imgWdt))
-      print(temp)
-      print(temp.ndim)
-      print(temp.shape)
-     
+
       for y in range(y1,y2):
           for x in range(x1,x2):
-              matrix[y-y1+1][x-x1+1][fr-strFrame+1]=temp[y][x]
-              #matrix=Image.fromarray(temp[y:x],'L')
-              #img.show()
-              
-              #print(temp(x,y))
-             #matrix(( y-y1+1, x-x1+1, fr-strFrame+1)) = temp ((y, x ))
-             
-             #plt.imshow(temp(x,y))
-             #plt.show()'''
-     
-      print(matrix)
-      
-      #plt.imshow(cv2.cvtColor(matrix, cv2.COLOR_BGR2RGB))
-      
-      #Img=Image.fromarray(np.uint8(matrix[..., fr-strFrame+1]),'L')
-      Img=Image.fromarray((matrix[..., fr-strFrame+1]),'RGB')
-      Img=Img.resize( [int(5 * s) for s in Img.size])
-      filename = "%d.jpg"%d
-      Img.save(filename)
-      d+=1
-FileConverterFxn('data_20190731_02.dat',64,64,150,2000,33,1,64,64)            
+              matrix[y-y1+1,x-x1+1,fr-strFrame+1]=temp[y,x]
+          
+
+
+  r=matrix[:,:,390]
+  plt.imshow(r,aspect='auto',interpolation='none',cmap='viridis', extent=[-0.5,31.5,63.5,-0.5] , origin='upper')
+  plt.savefig('plot.png')
+  print(r)
+  #print(np.shape(r))
+  #print(r)
+
+
+  #plt.imshow(r,extent=[0,100,0,100],aspect='auto')
+
+FileConverterFxn('data_20190731_01.dat',64,64,0,1000,33,1,64,64)            
       
   
 
